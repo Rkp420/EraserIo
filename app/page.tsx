087af03a6672f2159c7330f01fdfdf978dc4c319
Rkp420 <rkp20442@gmail.com>
2024-04-01 12:18:33 +0530

    SetUp Authentication using Kinde

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
-import Image from "next/image";
+"use client"
+
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
-    <div>Hello World!</div>
+    <div>
+      <Header />
+      <Hero />
+    </div>
   );
 }
